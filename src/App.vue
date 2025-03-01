fb9fe600f89d92da658950cc2733adde29cf9ef9
16lzh <liaozhenghong2022@163.com>
2025-03-01 23:19:51 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="basic-container">
+      <el-header class="basic-header">
+        <
+      </el-header>
+      <el-main class="basic-main">
+        Main
+      </el-main>
+      <el-footer class="basic-footer">
+        footer
+      </el-footer>
+  </div>
+</template>
+
+<style>
+.basic-container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: stretch;
+  align-content: stretch;
+  gap: 5px
+}
+
+.basic-header {
+  width: auto;
+  height: 120px;
+  order: 0;
+  alignSelf: auto;
+  flexBasis: auto;
+  flexGrow: 0;
+  flexShrink: 1;
+  zIndex: 99
+}
+
+.basic-main {
+  width: auto;
+  height: 700px;
+  order: 0;
+  alignSelf: auto;
+  flexBasis: auto;
+  flexGrow: 0;
+  flexShrink: 1;
+  zIndex: 99
+}
+
+.basic-footer {
+  width: auto;
+  height: 890px;
+  order: 0;
+  alignSelf: auto;
+  flexBasis: auto;
+  flexGrow: 0;
+  flexShrink: 1;
+  zIndex: 99
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'element-plus/dist/index.css'; // 确保引入 Element Plus 的样式文件
+
+createApp(App).mount('#app')
